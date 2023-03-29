cb3877b2531e6fe810d8f82674619a77c0347ab1
Shaileepatel2099 <shaileepatel2001@gmail.com>
2023-03-29 13:42:38 -0400

    change in code and design

--- TableViewAssign3/TableViewAssign3/SegmentTableViewCell.swift
+++ TableViewAssign3/TableViewAssign3/SegmentTableViewCell.swift
@@ -9,10 +9,26 @@ import UIKit
 
 class SegmentTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
+    @IBOutlet weak var segmentResult: UILabel!
+    
+    @IBOutlet weak var mySegment: UISegmentedControl!
+    
+    @IBAction func segmentchange(_ sender: Any) {
+        if (mySegment .selectedSegmentIndex == 0)
+            {
+                segmentResult.text = "First"
+            }
+            
+            else{
+                segmentResult.text = "Second"
+            }
+                
+        }
+        override func awakeFromNib() {
+            super.awakeFromNib()
+            // Initialization code
+        }
+    
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
